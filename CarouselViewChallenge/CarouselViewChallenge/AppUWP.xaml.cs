5dacb34a065d1d096a10ffb4d171b6f8ef76a2e2
David Ortinau <david.ortinau@microsoft.com>
2019-09-11 14:20:59 -0500

    added UWP head

---
+++ CarouselViewChallenge/CarouselViewChallenge/AppUWP.xaml.cs
@@ -0,0 +1,35 @@
+﻿using CarouselViewChallenge.Views;
+using Xamarin.Forms;
+
+namespace CarouselViewChallenge
+{
+    public partial class AppUWP : Application
+    {
+
+        public AppUWP()
+        {
+            InitializeComponent();
+
+            var tabs = new TabbedPage();
+            tabs.Children.Add(new NavigationPage(new WelcomePage()) { Title = "Welcome" });
+            tabs.Children.Add(new NavigationPage(new CarouselViewChallengePage()) { Title = "Carousel" });
+
+            MainPage = tabs;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
