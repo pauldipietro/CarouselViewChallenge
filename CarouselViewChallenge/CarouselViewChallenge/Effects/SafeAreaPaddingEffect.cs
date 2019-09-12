f4c81261b6d1a774cb3ce56c60aa4d20a4792d47
sthewissen <github@thewissen.io>
2019-09-12 11:36:19 +0200

    Remade Traveler sample

---
+++ CarouselViewChallenge/CarouselViewChallenge/Effects/SafeAreaPaddingEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CarouselViewChallenge.Effects
+{
+    public class SafeAreaPaddingEffect : RoutingEffect
+    {
+        public SafeAreaPaddingEffect() : base("CarouselViewChallenge.SafeAreaPaddingEffect")
+        {
+        }
+    }
+}
--- CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
+++ CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CarouselViewChallenge.Models;
+using CarouselViewChallenge.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,47 @@ namespace CarouselViewChallenge.Views
         public CarouselViewChallengePage()
         {
             InitializeComponent();
+
+            BindingContext = new CarouselViewChallengeViewModel
+            {
+                Destinations = new List<Destination>() {
+                 new Destination
+                 {
+                     Title = "Ulun Danu Beratan Temple",
+                     ImageUrl = "bali.jpg",
+                     Rating = 4.4f,
+                     Votes = 3829
+                 },
+                 new Destination
+                 {
+                     Title = "Isola d'Elba",
+                     ImageUrl = "elba.jpg",
+                     Rating = 4.9f,
+                     Votes = 9783
+                 }
+                },
+                Categories = new List<Category>()
+                {
+                    new Category
+                    {
+                        Color = Color.FromHex("#5d6dff"),
+                        Name = "Art &amp; Cultures",
+                        Icon = " "
+                    },
+                    new Category
+                    {
+                        Color = Color.FromHex("#37d2ff"),
+                        Name = "Adventure",
+                        Icon = " "
+                    },
+                    new Category
+                    {
+                        Color = Color.FromHex("#2687ee"),
+                        Name = "Some other thing",
+                        Icon = " "
+                    }
+                }
+            };
         }
     }
 }
