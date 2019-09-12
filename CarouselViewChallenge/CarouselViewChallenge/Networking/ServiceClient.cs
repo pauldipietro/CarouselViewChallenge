b6b140a3d59e2989b43f8e6da5b3da37491b8f41
Samir GC <55045516+samirgcofficial@users.noreply.github.com>
2019-09-12 11:13:48 +0530

    Add files via upload

---
+++ CarouselViewChallenge/CarouselViewChallenge/Networking/ServiceClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using CarouselViewChallenge.Models;
+
+namespace CarouselViewChallenge.Networking
+{
+    public class ServiceClient
+    {
+        private static ServiceClient _ServiceClientInstance;
+
+        public static ServiceClient ServiceClientInstance
+        {
+            get
+            {
+                if (_ServiceClientInstance == null)
+                    _ServiceClientInstance = new ServiceClient();
+                return _ServiceClientInstance;
+            }
+        }
+        public List<ConsultancyModel> GetConsultancyInformationPosts()
+        {
+            return new List<ConsultancyModel>
+            {
+                new ConsultancyModel { Title = "Turtle Bay Resort", Date = "JULY 24, 2015",  Image = "hawaii.jpg" },
+                new ConsultancyModel { Title = "Grand Hotel", Date = "JULY 24, 2015", Image = "michigan.jpg" },
+                new ConsultancyModel { Title = "Vista Verde", Date = "JULY 23, 2015", Image = "Colorado.jpg" },
+                new ConsultancyModel { Title = "Woodloch Pines", Date = "JULY 23, 2015", Image = "hawaii.jpg" },
+            };
+        }
+    }
+}
