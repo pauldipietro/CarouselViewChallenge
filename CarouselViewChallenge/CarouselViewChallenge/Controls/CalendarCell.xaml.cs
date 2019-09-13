f394f51b42409d3da8d684cb875522f9240e9304
Trevor Tirrell <ttirrell@hotmail.com>
2019-09-12 22:53:34 -0400

    Added CalendarCell Custom Control

---
+++ CarouselViewChallenge/CarouselViewChallenge/Controls/CalendarCell.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselViewChallenge.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalendarCell : ContentView
+    {
+        public string Date { get; set; }
+        public static readonly BindableProperty DateTextProperty = BindableProperty.Create(
+                                                         propertyName: "Date",
+                                                         returnType: typeof(string),
+                                                         declaringType: typeof(CalendarCell),
+                                                         defaultValue: "",
+                                                         defaultBindingMode: BindingMode.TwoWay,
+                                                         propertyChanged: DateTextPropertyChanged);
+
+        private static void DateTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CalendarCell)bindable;
+            control.Date = newValue.ToString();
+        }
+
+        public CalendarCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
