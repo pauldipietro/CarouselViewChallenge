f3c56d2d92a2bd8ca5c0fde3cf848320cb87f167
Lisa Bowden <Lisa.Bowden@blacklightsoftware.com>
2019-09-13 17:36:00 +0100

    started looking at getting rss feeds

---
+++ CarouselViewChallenge/CarouselViewChallenge/Services/Abstract/IPodcastService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarouselViewChallenge.Models;
+
+namespace CarouselViewChallenge.Services.Abstract
+{
+    public interface IPodcastService
+    {
+        Task<IEnumerable<Podcast>> GetPodcastsAsync();
+    }
+}
---
+++ CarouselViewChallenge/CarouselViewChallenge/Services/Concrete/PodcastService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using CarouselViewChallenge.Models;
+using CarouselViewChallenge.Services.Abstract;
+
+namespace CarouselViewChallenge.Services.Concrete
+{
+    public class PodcastService : IPodcastService
+    {
+        List<string> _podcastFeeds = new List<string>()
+        {
+            "https://feeds.fireside.fm/xamarinpodcast/rss",
+            "https://pinecast.com/feed/ladybug-podcast",
+            "https://feeds.simplecast.com/gvtxUiIf",
+            "https://rss.art19.com/buffering-the-vampire-slayer"
+        };
+
+        public async Task<IEnumerable<Podcast>> GetPodcastsAsync()
+        {
+            var podcasts = new List<Podcast>();
+
+            foreach (var feedUrl in _podcastFeeds)
+            {
+                podcasts.Add(await GetEpisodes(feedUrl));
+            }
+
+            return podcasts;
+        }
+
+        private async Task<Podcast> GetEpisodes(string feedUrl)
+        {
+            Podcast cast = new Podcast();
+
+            var xml = XDocument.Load(feedUrl);
+            cast.Description = xml("description");
+            return cast;
+        }
+    }
+}
