33bf89f2d388017bd0370c1b18262e70ac245c83
irongut <murray.dave@outlook.com>
2019-09-14 22:58:04 +0100

    initial UI layout

---
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewChallengeViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CarouselViewChallenge.ViewModels
+{
+    public class CarouselViewChallengeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ObservableCollection<string> GalNetNewsList { get; set; }
+
+        private DateTime _galnetLastUpdated;
+        public DateTime GalNetLastUpdated
+        {
+            get { return _galnetLastUpdated; }
+            private set
+            {
+                if (_galnetLastUpdated != value)
+                {
+                    _galnetLastUpdated = value;
+                    OnPropertyChanged(nameof(GalNetLastUpdated));
+                }
+            }
+        }
+
+        public CarouselViewChallengeViewModel()
+        {
+            GalNetNewsList = new ObservableCollection<string>
+            {
+                "First Item",
+                "Second Item",
+                "Third Item"
+            };
+            GalNetLastUpdated = DateTime.Now;
+        }
+    }
+}
--- CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
+++ CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using CarouselViewChallenge.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +7,12 @@ namespace CarouselViewChallenge.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CarouselViewChallengePage : ContentPage
     {
+        private readonly CarouselViewChallengeViewModel vm = new CarouselViewChallengeViewModel();
+
         public CarouselViewChallengePage()
         {
             InitializeComponent();
+            BindingContext = vm;
         }
     }
 }
