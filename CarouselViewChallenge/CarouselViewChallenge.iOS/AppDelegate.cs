a5ee4b9eb994cfedc30eff70d4d79f6aa069a8f2
Nicolas Garcia <nicolas-garcia@users.noreply.github.com>
2019-09-14 21:20:49 +0200

    Basic XKCD viewer using CarouselView.

--- CarouselViewChallenge/CarouselViewChallenge.iOS/AppDelegate.cs
+++ CarouselViewChallenge/CarouselViewChallenge.iOS/AppDelegate.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿using FFImageLoading.Forms.Platform;
 using Foundation;
 using UIKit;
 
@@ -23,6 +20,7 @@ namespace CarouselViewChallenge.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
+            CachedImageRenderer.Init();
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
