33d8d918d5276889dda519e5db1bb8b7582b7341
Thinh Tran <thinhtran@outlook.com.au>
2019-09-15 11:47:47 +0800

    Create Woolworths money app clone

---
+++ CarouselViewChallenge/CarouselViewChallenge/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CarouselViewChallenge.Models
+{
+    public class Card
+    {
+        public string CardImage { get; set; }
+        public float OpeningBalance { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public List<Detail> Details { get; set; }
+    };
+
+    public class Detail
+    {
+        public string Icon { get; set; }
+        public string DetailTitle { get; set; }
+        public string DetailFact { get; set; }
+    }
+}
