d80f7097a9537fbd317c3b657ed95e0100cb9bf1
Israel Pérez Saucedo <neomatrixisra25@hotmail.com>
2019-09-17 15:51:02 -0500

    Challenge completition
    My challenge  version :)

---
+++ CarouselViewChallenge/CarouselViewChallenge/Model/PhotoResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CarouselViewChallenge.Model
+{
+    public class PhotoResponse
+    {
+        public int total { get; set; }
+
+        public int total_pages { get; set; }
+
+        public PhotoInfo[] results { get; set; }
+    }
+}
---
+++ CarouselViewChallenge/CarouselViewChallenge/Model/PhotoUrls.cs
@@ -0,0 +1,10 @@
+﻿namespace CarouselViewChallenge.Model
+{
+    public class PhotoUrls
+    {
+        public string full { get; set; }
+        public string regular { get; set; }
+        public string small { get; set; }
+        public string thumb { get; set; }
+    }
+}
