d007fc7c44399bd28f51aa53eb1fb03ef539f5d1
David Ortinau <david.ortinau@microsoft.com>
2019-09-16 21:18:32 -0500

    an infinite looping carousel

--- CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
+++ CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
@@ -16,5 +16,22 @@ namespace CarouselViewChallenge.Views
         {
             InitializeComponent();
         }
+
+        public void OnPositionChanged(object sender, PositionChangedEventArgs args)
+        {
+            var max = 5;
+            if(args.CurrentPosition == max)
+            {
+                carousel.IsScrollAnimated = false;
+                carousel.Position = 1;
+                carousel.IsScrollAnimated = true;
+            }
+            else if(args.CurrentPosition == 0)
+            {
+                carousel.IsScrollAnimated = false;
+                carousel.Position = 4;
+                carousel.IsScrollAnimated = true;
+            }
+        }
     }
 }
