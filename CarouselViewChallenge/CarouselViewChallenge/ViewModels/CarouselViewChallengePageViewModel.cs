663c37cb567b7ab15b5273a93c7d744b874ae666
Lisa Bowden <Lisa.Bowden@blacklightsoftware.com>
2019-09-20 18:28:26 +0100

    set the binding context - idiot!

--- CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewChallengePageViewModel.cs
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewChallengePageViewModel.cs
@@ -15,11 +15,11 @@ namespace CarouselViewChallenge.ViewModels
 
         public CarouselViewChallengePageViewModel()
         {
-            //StartingLetters = new ObservableCollection<string>()
-            //{
-            //    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-            //    "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-            //};
+            StartingLetters = new ObservableCollection<string>()
+            {
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+                "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+            };
 
             _monsterService = new FakeMonsterService();
             Monsters = _monsterService.GetMonsters();
--- CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
+++ CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
@@ -8,23 +8,13 @@ namespace CarouselViewChallenge.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CarouselViewChallengePage : ContentPage
     {
-        public CarouselViewChallengePageViewModel vm { get; set; }
+        public CarouselViewChallengePageViewModel Vm { get; set; }
 
         public CarouselViewChallengePage()
         {
             InitializeComponent();
-            vm = new CarouselViewChallengePageViewModel();
-            //SetupData();
-        }
-
-        private void SetupData()
-        {
-            vm.StartingLetters = new ObservableCollection<string>()
-            {
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-                "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-            };
-            vm.SelectedLetter = "H";
+            Vm = new CarouselViewChallengePageViewModel();
+            BindingContext = Vm;
         }
     }
 }
