6177cfd0fb3abf40c47590f477a84fde2272a3bf
irongut <murray.dave@outlook.com>
2019-09-20 23:50:09 +0100

    initial UI layout for Fortify page

---
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewTwoViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace CarouselViewChallenge.ViewModels
+{
+    public class CarouselViewTwoViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ObservableCollection<string> LargeShipList { get; set; }
+
+        public ObservableCollection<string> SmallShipList { get; set; }
+
+        public string _ccBalance;
+        public string CCBalance
+        {
+            get { return _ccBalance; }
+            private set
+            {
+                if (_ccBalance != value)
+                {
+                    _ccBalance = value;
+                    OnPropertyChanged(nameof(CCBalance));
+                }
+            }
+        }
+
+        private string _cycle;
+        public string Cycle
+        {
+            get { return _cycle; }
+            protected set
+            {
+                if (_cycle != value)
+                {
+                    _cycle = value;
+                    OnPropertyChanged(nameof(Cycle));
+                }
+            }
+        }
+
+        private DateTime _lastUpdated;
+        public DateTime LastUpdated
+        {
+            get { return _lastUpdated; }
+            private set
+            {
+                if (_lastUpdated != value)
+                {
+                    _lastUpdated = value;
+                    OnPropertyChanged(nameof(LastUpdated));
+                }
+            }
+        }
+
+        public CarouselViewTwoViewModel()
+        {
+            LargeShipList = new ObservableCollection<string>
+            {
+                "First Large Item",
+                "Second Large Item",
+                "Third Large Item",
+                "Fourth Large Item",
+                "Fifth Large Item",
+                "Sixth Large Item",
+                "Seventh Large Item",
+                "Eighth Large Item",
+                "Ninth Large Item",
+                "Tenth Large Item"
+            };
+            SmallShipList = new ObservableCollection<string>
+            {
+                "First Small Item",
+                "Second Small Item",
+                "Third Small Item",
+                "Fourth Small Item",
+                "Fifth Small Item",
+                "Sixth Small Item",
+                "Seventh Small Item",
+                "Eighth Small Item",
+                "Ninth Small Item",
+                "Tenth Small Item"
+            };
+            Cycle = "Cycle 150";
+            CCBalance = "853 CC";
+            LastUpdated = DateTime.Now;
+        }
+    }
+}
---
+++ CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewTwoPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CarouselViewChallenge.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselViewChallenge.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarouselViewTwoPage : ContentPage
+    {
+        private readonly CarouselViewTwoViewModel vm = new CarouselViewTwoViewModel();
+
+        public CarouselViewTwoPage()
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+    }
+}
