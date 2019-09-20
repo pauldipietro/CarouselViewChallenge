9f5fe8b6d74be4858cb5e6d72a55b75d385ec033
sthewissen <github@thewissen.io>
2019-09-21 00:36:17 +0200

    Initial port

--- CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewChallengeViewModel.cs
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewChallengeViewModel.cs
@@ -6,7 +6,6 @@ namespace CarouselViewChallenge.ViewModels
 {
     public class CarouselViewChallengeViewModel
     {
-        public List<Destination> Destinations { get; set; } = new List<Destination>();
-        public List<Category> Categories { get; set; } = new List<Category>();
+        public List<CarouselItem> Items { get; set; } = new List<CarouselItem>();
     }
 }
--- CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
+++ CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
@@ -13,50 +13,86 @@ namespace CarouselViewChallenge.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CarouselViewChallengePage : ContentPage
     {
+        private List<Color> _backgroundColors = new List<Color>();
+
         public CarouselViewChallengePage()
         {
             InitializeComponent();
 
-            BindingContext = new CarouselViewChallengeViewModel
+            var model = new CarouselViewChallengeViewModel
             {
-                Destinations = new List<Destination>() {
-                 new Destination
-                 {
-                     Title = "Ulun Danu Beratan Temple",
-                     ImageUrl = "bali.jpg",
-                     Rating = 4.4f,
-                     Votes = 3829
-                 },
-                 new Destination
-                 {
-                     Title = "Isola d'Elba",
-                     ImageUrl = "elba.jpg",
-                     Rating = 4.9f,
-                     Votes = 9783
-                 }
-                },
-                Categories = new List<Category>()
+                Items = new List<CarouselItem>()
                 {
-                    new Category
-                    {
-                        Color = Color.FromHex("#5d6dff"),
-                        Name = "Art & Cultures",
-                        Icon = " "
-                    },
-                    new Category
-                    {
-                        Color = Color.FromHex("#37d2ff"),
-                        Name = "Adventure",
-                        Icon = " "
-                    },
-                    new Category
-                    {
-                        Color = Color.FromHex("#2687ee"),
-                        Name = "Some other thing",
-                        Icon = " "
-                    }
+                    // Just create some dummy data here for now.
+                    new CarouselItem{ Type="JUICY AND ORANGE", ImageSrc="oranges.png", Name = "ORANGE AWESOMENESS", Price = 120, Title = "ORANGE AWESOMENESS", BackgroundColor= Color.FromHex("#9866d5"), StartColor=Color.FromHex("#f3463f"),  EndColor=Color.FromHex("#fece49")},
+                    new CarouselItem{ Type="NOT A TYPICAL FRUIT", ImageSrc="tomato.png", Name = "TERRIBLE TOMATO", Price = 129, Title = "TERRIBLE TOMATO", BackgroundColor= Color.FromHex("#fab62a"), StartColor=Color.FromHex("#42a7ff"),  EndColor=Color.FromHex("#fab62a")},
+                    new CarouselItem{ Type="SWEET AND GREEN", ImageSrc="pear.png", Name = "PEAR PARTY", Price = 140, Title = "PEAR PARTY", BackgroundColor= Color.FromHex("#425cfc"), StartColor=Color.FromHex("#33ccf3"),  EndColor=Color.FromHex("#ccee44")}
                 }
             };
+
+            BindingContext = model;
+
+            // Create out a list of background colors based on our items colors so we can do a gradient on scroll.
+            for (int i = 0; i < model.Items.Count; i++)
+            {
+                var current = model.Items[i];
+                var next = model.Items.Count > i + 1 ? model.Items[i + 1] : null;
+
+                if (next != null)
+                    _backgroundColors.AddRange(SetGradients(current.BackgroundColor, next.BackgroundColor, 375));
+                else
+                    _backgroundColors.Add(current.BackgroundColor);
+            }
+        }
+
+        public void Handle_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+        {
+            debug.Text = $"FVII:{e.FirstVisibleItemIndex} | LVII:{e.LastVisibleItemIndex} | HD:{e.HorizontalDelta:0.000} | HO:{e.HorizontalOffset:0.000}";
+
+            // var fullWidth = carousel.Width;
+
+            var position = e.HorizontalOffset;
+
+            // Set the background color of our page to the item in the color gradient
+            // array, matching the current scrollindex.
+            if (position > _backgroundColors.Count - 1)
+                page.BackgroundColor = _backgroundColors.Last();
+            else if (position < 0)
+                page.BackgroundColor = _backgroundColors.First();
+            else
+                page.BackgroundColor = _backgroundColors[(int)position];
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Need to start somewhere...
+            page.BackgroundColor = _backgroundColors.First();
+        }
+
+        // Create a list of all the colors in between our start and end color.
+        public static IEnumerable<Color> SetGradients(Color start, Color end, int steps)
+        {
+            var colorList = new List<Color>();
+
+            double aStep = ((end.A * 255) - (start.A * 255)) / steps;
+            double rStep = ((end.R * 255) - (start.R * 255)) / steps;
+            double gStep = ((end.G * 255) - (start.G * 255)) / steps;
+            double bStep = ((end.B * 255) - (start.B * 255)) / steps;
+
+            for (int i = 0; i < steps; i++)
+            {
+                var a = (start.A * 255) + (int)(aStep * i);
+                var r = (start.R * 255) + (int)(rStep * i);
+                var g = (start.G * 255) + (int)(gStep * i);
+                var b = (start.B * 255) + (int)(bStep * i);
+
+                colorList.Add(Color.FromRgba(r / 255, g / 255, b / 255, a / 255));
+            }
+
+            return colorList;
         }
     }
 }
