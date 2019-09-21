bc65863cbe617343bb04a9b6c046aea2dda9b6c5
irongut <murray.dave@outlook.com>
2019-09-21 20:50:03 +0100

    show errors with a Toast because EmptyView not working

--- CarouselViewChallenge/CarouselViewChallenge.Android/MainActivity.cs
+++ CarouselViewChallenge/CarouselViewChallenge.Android/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using Acr.UserDialogs;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
@@ -21,6 +22,7 @@ namespace CarouselViewChallenge.Droid
 
             CachedImageRenderer.Init(true);
             var ignore = typeof(SvgCachedImage);
+            UserDialogs.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
 
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
