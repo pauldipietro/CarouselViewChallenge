282df22fa4ec4b5187d2937fc643373b7ca704cd
irongut <murray.dave@outlook.com>
2019-09-21 01:17:15 +0100

    implement new Fortify UI

--- CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewTwoViewModel.cs
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewTwoViewModel.cs
@@ -68,29 +68,29 @@ namespace CarouselViewChallenge.ViewModels
             // fake data
             LargeShipList = new ObservableCollection<FortItem>
             {
-                new FortItem("Xinca", "101 ly", "station", "343 ls", "independent"),
-                new FortItem("Ngarawe", "86 ly", "station", "35 ls", "empire"),
-                new FortItem("Yanerones", "115 ly", "planetary", "42 ls", "empire"),
-                new FortItem("Damoorai", "86 ly", "station", "1,324 ls", "empire"),
-                new FortItem("Ostyat", "117 ly", "station", "41 ls", "independent"),
-                new FortItem("Itzamni", "64 ly", "planetary", "158,992 ls", "empire"),
-                new FortItem("HIP 20524", "53 ly", "planetary", "2,192 ls", "empire"),
-                new FortItem("Amenta", "47 ly", "station", "236 ls", "empire"),
-                new FortItem("27 G. Caeli", "54 ly", "outpost", "3,145 ls", "independent"),
-                new FortItem("Kappa Reticuli", "81 ly", "planetary", "350 ls", "federation")
+                new FortItem("Xinca", "101 ly", "Station", "343 ls", "independent"),
+                new FortItem("Ngarawe", "86 ly", "Station", "35 ls", "empire"),
+                new FortItem("Yanerones", "115 ly", "Planetary", "42 ls", "empire"),
+                new FortItem("Damoorai", "86 ly", "Station", "1,324 ls", "empire"),
+                new FortItem("Ostyat", "117 ly", "Station", "41 ls", "independent"),
+                new FortItem("Itzamni", "64 ly", "Planetary", "158,992 ls", "empire"),
+                new FortItem("HIP 20524", "53 ly", "Planetary", "2,192 ls", "empire"),
+                new FortItem("Amenta", "47 ly", "Station", "236 ls", "empire"),
+                new FortItem("27 G. Caeli", "54 ly", "Outpost", "3,145 ls", "independent"),
+                new FortItem("Kappa Reticuli", "81 ly", "Planetary", "350 ls", "federation")
             };
             SmallShipList = new ObservableCollection<FortItem>
             {
-                new FortItem("Lutni", "30 ly", "planetary", "21 ls", "empire"),
-                new FortItem("AB Pictoris", "41 ly", "station", "1,739 ls", "empire"),
-                new FortItem("HIP 21778", "66 ly", "outpost", "181 ls", "independent"),
-                new FortItem("Biliri", "68 ly", "outpost", "181 ls", "empire"),
-                new FortItem("Ngarawe", "86 ly", "station", "35 ls", "empire"),
-                new FortItem("Lopocares", "125 ly", "outpost", "329 ls", "federation"),
-                new FortItem("Carverda", "41 ly", "station", "727 ls", "empire"),
-                new FortItem("HIP 32812", "62 ly", "outpost", "1,753 ls", "federation"),
-                new FortItem("Tiburnat", "53 ly", "planetary", "2,192 ls", "empire"),
-                new FortItem("Ju Shiva", "47 ly", "station", "236 ls", "independent"),
+                new FortItem("Lutni", "30 ly", "Planetary", "21 ls", "empire"),
+                new FortItem("AB Pictoris", "41 ly", "Station", "1,739 ls", "empire"),
+                new FortItem("HIP 21778", "66 ly", "Outpost", "181 ls", "independent"),
+                new FortItem("Biliri", "68 ly", "Outpost", "181 ls", "empire"),
+                new FortItem("Ngarawe", "86 ly", "Station", "35 ls", "empire"),
+                new FortItem("Lopocares", "125 ly", "Outpost", "329 ls", "federation"),
+                new FortItem("Carverda", "41 ly", "Station", "727 ls", "empire"),
+                new FortItem("HIP 32812", "62 ly", "Outpost", "1,753 ls", "federation"),
+                new FortItem("Tiburnat", "53 ly", "Planetary", "2,192 ls", "empire"),
+                new FortItem("Ju Shiva", "47 ly", "Station", "236 ls", "independent"),
             };
             Cycle = "Cycle 150";
             CCBalance = "853 CC";
