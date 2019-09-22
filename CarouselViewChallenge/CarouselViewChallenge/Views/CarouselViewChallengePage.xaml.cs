4a85ad06e2bd92ee02f12ec7e59f039115d29e53
Brice Friha <brice.friha@aol.fr>
2019-09-22 22:48:17 +0200

    s

--- CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
+++ CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
@@ -20,9 +20,53 @@ namespace CarouselViewChallenge.Views
             BindingContext = new ListGamesViewModel();
         }
 
-        private async void CarouselView_Focused(object sender, FocusEventArgs e)
+        private void CarouselView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
         {
-            
+            //var carousel = (CarouselView)sender;
+            //var carouselItems = carousel.ItemsSource.Cast<object>().ToList();
+            //var currentIndex = e.CenterItemIndex;
+            //var lastIndex = e.LastVisibleItemIndex;
+            //var layout = carousel.ItemsLayout;
+            //var adjust = Device.RuntimePlatform == Device.Android ? 3 : 1;
+
+            //if (layout is ListItemsLayout listItemsLayout)
+            //{
+            //    if (listItemsLayout.Orientation == ItemsLayoutOrientation.Horizontal)
+            //    {
+            //        var carouselWidth = carousel.Width;
+            //        var offset = (carouselWidth * (currentIndex + 1)) - (e.HorizontalOffset / adjust);
+            //        var position = (offset * ParallaxOffset / carouselWidth) - ParallaxOffset;
+            //        var scale = (offset * 100 / carouselWidth) / 100;
+
+            //        var lastItem = carouselItems[lastIndex] as CarouselItem;
+            //        lastItem.Position = position + ParallaxOffset;
+            //        lastItem.Rotation = position / 2;
+            //        lastItem.Scale = 1 - scale;
+
+            //        var currentItem = carouselItems[currentIndex] as CarouselItem;
+            //        currentItem.Position = position;
+            //        currentItem.Rotation = position;
+            //        currentItem.Scale = scale;
+            //    }
+
+            //    if (listItemsLayout.Orientation == ItemsLayoutOrientation.Vertical)
+            //    {
+            //        var carouselHeight = carousel.Height;
+            //        var offset = (carouselHeight * (currentIndex + 1)) - (e.VerticalOffset / adjust);
+            //        var position = (offset * ParallaxOffset / carouselHeight) - ParallaxOffset;
+            //        var scale = offset * 100 / carouselHeight / 100;
+
+            //        var lastItem = carouselItems[lastIndex] as CarouselItem;
+            //        lastItem.Position = position + ParallaxOffset;
+            //        lastItem.Rotation = position / 2;
+            //        lastItem.Scale = 1 - scale;
+
+            //        var currentItem = carouselItems[currentIndex] as CarouselItem;
+            //        currentItem.Position = position;
+            //        currentItem.Rotation = position;
+            //        currentItem.Scale = scale;
+            //    }
+            //}
         }
     }
 }
