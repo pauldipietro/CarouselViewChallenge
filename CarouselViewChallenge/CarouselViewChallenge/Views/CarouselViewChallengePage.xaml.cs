5dd235640b12fe011e72041331f9ee1e13633c54
Depechie <glenn.versweyveld@gmail.com>
2019-09-23 23:50:21 +0200

    Car config app

--- CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
+++ CarouselViewChallenge/CarouselViewChallenge/Views/CarouselViewChallengePage.xaml.cs
@@ -1,20 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace CarouselViewChallenge.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CarouselViewChallengePage : ContentPage
+    public partial class CarouselViewChallengePage : ContentPage, INotifyPropertyChanged
     {
+        private ObservableCollection<string> _images = new ObservableCollection<string>();
+        public ObservableCollection<string> Images
+        {
+            get => _images;
+            set
+            {
+                _images = value;
+                OnPropertyChanged(nameof(Images));
+            }
+        }
+
         public CarouselViewChallengePage()
         {
             InitializeComponent();
+
+            p1.BackgroundColor = Color.FromRgba(255, 255, 255, 0.7);
+            p2.BackgroundColor = Color.FromRgba(255, 255, 255, 0.7);
+
+            Images.Add("le_00.jpg");
+            Images.Add("le_01.jpg");
+            BindingContext = this;
         }
     }
 }
