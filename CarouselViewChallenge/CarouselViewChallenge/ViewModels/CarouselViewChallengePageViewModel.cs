3b004b3ecb3c10ba89bebeea8a949b3fbd39987c
leandrosdias <leandrosdias0@gmail.com>
2019-09-24 16:48:43 -0300

    Update CarouselViewChallengePageViewModel.cs
    Create list of story cards in view model

--- CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewChallengePageViewModel.cs
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewChallengePageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CarouselViewChallenge.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
 
@@ -8,6 +10,23 @@ namespace CarouselViewChallenge.ViewModels
     class CarouselViewChallengePageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<StoryCard> _storyCards;
+        public ObservableCollection<StoryCard> StoryCards
+        {
+            get
+            {
+                return _storyCards;
+            }
+            set
+            {
+                if (_storyCards != value)
+                {
+                    _storyCards = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("StoryCards"));
+                }
+            }
+        }
 
         private void OnPropertyChanged(PropertyChangedEventArgs eventArgs)
         {
