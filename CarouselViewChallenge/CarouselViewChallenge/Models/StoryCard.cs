9bcd297504404f4a0d9041392048d8833892d41f
leandrosdias <leandrosdias0@gmail.com>
2019-09-24 14:38:59 -0300

    Create model StoryCard
    Create story card model

---
+++ CarouselViewChallenge/CarouselViewChallenge/Models/StoryCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarouselViewChallenge.Models
+{
+    public class StoryCard
+    {
+        public string ProfileImage { get; set; }
+        public string Name { get; set; }
+        public string StoryImage { get; set; }
+        public bool IsUserStory { get; set; }
+    }
+}
