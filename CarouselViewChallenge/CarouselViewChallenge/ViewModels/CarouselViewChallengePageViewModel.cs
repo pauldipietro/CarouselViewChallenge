60d24b9d6c8c08f3146f536bcd5653c5f69502cf
leandrosdias <leandrosdias0@gmail.com>
2019-09-24 14:31:38 -0300

    Create CarouselViewChallengePageViewModel.cs
    Create ViewModel page

---
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselViewChallengePageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CarouselViewChallenge.ViewModels
+{
+    class CarouselViewChallengePageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(PropertyChangedEventArgs eventArgs)
+        {
+            PropertyChanged?.Invoke(this, eventArgs);
+        }
+    }
+}
