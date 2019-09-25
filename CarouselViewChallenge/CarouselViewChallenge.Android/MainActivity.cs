fecacfdf7c0be69bb23f35150e36f78ad01621a4
Eoghan McCarroll <eoghanmccarroll@gmail.com>
2019-09-24 22:28:41 -0400

    Commiting additions for CarouselViewChallenge

--- CarouselViewChallenge/CarouselViewChallenge.Android/MainActivity.cs
+++ CarouselViewChallenge/CarouselViewChallenge.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Platform;
 
 namespace CarouselViewChallenge.Droid
 {
@@ -20,8 +21,10 @@ namespace CarouselViewChallenge.Droid
             base.OnCreate(savedInstanceState);
 
             global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CachedImageRenderer.InitImageViewHandler();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/BaseViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CarouselViewChallenge.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        // public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>() ?? new MockDataStore();
+        public event PropertyChangedEventHandler PropertyChanged;
+        string _propTitle = string.Empty;
+        bool _propIsBusy;
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return _propIsBusy; }
+            set { SetProperty(ref _propIsBusy, value, "IsBusy"); }
+        }
+
+        string title = string.Empty;
+        public string Title
+        {
+            get { return _propTitle; }
+            set { SetProperty(ref _propTitle, value, "Title"); }
+        }
+
+        protected void SetProperty<T>(ref T store, T value, string propName, Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(store, value))
+                return;
+            store = value;
+            if (onChanged != null)
+                onChanged();
+            OnPropertyChanged(propName);
+        }
+
+        //#region INotifyPropertyChanged
+        //public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged == null)
+                return;
+            PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+        //#endregion
+    }
+}
