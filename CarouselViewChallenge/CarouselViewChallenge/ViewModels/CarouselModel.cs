f8fac69268d03e9204c460cb7955f730224f85fd
Pavithra <pavithras@rajasri.net>
2019-10-09 18:19:15 +0530

    carouselview challenge added

---
+++ CarouselViewChallenge/CarouselViewChallenge/ViewModels/CarouselModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace CarouselViewChallenge.ViewModels
+{
+    public class Sample
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+    public class CarouselModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Sample> _sections;
+        public ObservableCollection<Sample> Sections
+        {
+            get
+            {
+                return _sections;
+            }
+            set
+            {
+                if (_sections != value)
+                {
+                    _sections = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("Sections"));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(PropertyChangedEventArgs eventArgs)
+        {
+            PropertyChanged?.Invoke(this, eventArgs);
+        }
+    }
+}
